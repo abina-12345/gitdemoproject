681f13880fb56bd22ad7b21c8ec2f77f106d26dd
djavi <djavi@LAPTOP-OQ0IM35U>
2022-03-14 20:58:03 +0530

    my first commit to local repository

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+    String x="ufbukfkgygiug^1234iluhgiygh5678";
+    String y=x.replaceAll("[^0-9]","");
+    System.out.println(y);
+
+	}
+
+}
